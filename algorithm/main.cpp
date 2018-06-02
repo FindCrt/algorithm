9b8030ee2d1e8f943c91fe3fe6d05dacb7bc8590
shiwei <15811842975@163.com>
2018-06-02 18:03:31 +0800

    总结文档

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -793,6 +793,312 @@ int flipDigit(vector<int> &nums) {
     return opera;
 }
 
+//从16开始，就是从k-5复制4次,增长了维持在1.25-1.33
+int maxA(int N) {
+    vector<int> dp(N + 1, 0);
+    for (int i = 0; i <= N; ++i) {
+        dp[i] = i;
+        int maxj = 1;
+        for (int j = 1; j < i - 2; ++j) {
+            int cur = dp[j] * (i - j - 1);
+            if (cur>dp[i]) {
+                dp[i] = cur;
+                maxj = j;
+            }
+        }
+        
+        printf("%d->%d[%d] %d rate:%.3f\n",i,maxj,i-maxj,dp[i],dp[i]/(float)dp[i-1]);
+    }
+    return dp[N];
+}
+
+//869. 找出一个数组的错乱
+//1: 当4换到1的位置，1不在4的位置时，这时的错乱情况可以看成原始状态为231的序列错了乱，因为231分别都不能在这样的位置上，而它的排列情况等价于123
+//2. 缺乏对大数乘法求余的算法，这题解不出来
+int findDerangement(int n) {
+    if (n<3) {
+        if (n==1) {
+            return 0;
+        }else if (n==2){
+            return 1;
+        }
+    }
+    int limit = 1000000007;
+    int res1 = 0, res2 = 1;
+    for (int i = 3; i<=n; i++) {
+        int base = (res1+res2)%limit;
+       
+        int res = base;
+        int rate = limit/base;
+        if (rate<i-1) {
+            int j = 0;
+            for (; j<i-1; j+=rate) {
+                res += base*rate;
+                res %= limit;
+            }
+            res += base*(i-j-rate);
+            res %= limit;
+            
+        }else{
+            res = base*(i-1);
+        }
+        
+        res1 = res2;
+        res2 = res;
+        
+        printf("%d %d\n",i,res2);
+    }
+    
+    return res2;
+}
+
+//871. 最小分解
+int smallestFactorization(int a) {
+    int factors[8];
+    memset(factors, 0, sizeof(factors));
+    
+    for (int i = 9; i>1; i--) {
+        while (a%i==0) {
+            factors[i-2]++;
+            a /= i;
+        }
+        if (a == 1) {
+            break;
+        }
+    }
+    
+    if (a != 1) {
+        return 0;
+    }
+    
+    int result = 0;
+    int bit = 1;
+    int overCount = 0, overBit = 1;
+    for (int i = 9; i>1; i--) {
+        int j = factors[i-2];
+        while (j>0) {
+            if (bit == 1000000000) { //当位权值已经到了int最大位的时候，从新开始统计，即分成两部分：小于1000000000的值大小和大于这个值的大小。
+                overCount += i*overBit;
+                if (overCount > 2) { //int最大值最高位是2
+                    return 0;
+                }
+                overBit *= 10;
+                
+            }else{
+                result += i*bit;
+                bit *= 10;
+            }
+            
+            j--;
+        }
+    }
+    
+    if (overCount == 2) {
+        if (INT_MAX-2000000000 < result) {
+            return 0;
+        }
+    }
+    
+    return result;
+}
+
+inline int killProcessFind(vector<int> &ppid, int target){
+    int cur = (int)ppid.size()-1;
+    while (ppid[cur] != target) {
+        cur--;
+    }
+    
+    return cur;
+}
+
+//杀死进程，题目是要返回列表，这里的解法是求最后一个被杀死的
+int killProcess(vector<int> &pid, vector<int> &ppid, int kill) {
+    
+    int target = kill;
+    while (1) {
+        int i = killProcessFind(ppid, target);
+        if (i < 0) {
+            return target;
+        }
+        target = pid[i];
+    }
+}
+
+//873. 模拟松鼠
+//看起来难，其实很简单，每次拿一个，每个坚果的任务是独立的，除了第一次，唯一的优化点就是第一次拿哪一个坚果。
+int minDistance(int height, int width, vector<int> &tree, vector<int> &squirrel, vector<vector<int>> &nuts) {
+    int result = 0;
+    int treeX = tree.front(), treeY = tree.back();
+    int squX = squirrel.front(), squY = squirrel.back();
+    
+    int minSquDiff = INT_MAX;
+    for (auto &nut:nuts){
+        int treeLen = abs(treeX-nut.front())+abs(treeY-nut.back());
+        int squLen = abs(squX-nut.front())+abs(squY-nut.back());
+        
+        result += 2*treeLen;
+        minSquDiff = min(minSquDiff, squLen-treeLen);
+    }
+    
+    return result+minSquDiff;
+}
+
+//所有其他点和本点的连线，都在相邻连线构成的小于180度的那一边。
+//或者从左边连线每个点一直到右边，角度变化的方向是不变的
+bool isConvex(vector<vector<int>> &point) {
+    float slope[point.size()][point.size()];
+    for (int i = 0; i<point.size(); i++) {
+        memset(slope[i], 0, sizeof(slope[i]));
+    }
+    
+    for (int i = 0; i<point.size();i++){
+        auto &p1 = point[i];
+        for (int j = i+1;j<point.size();j++){
+            auto &p2 = point[j];
+            
+            float diffX = p2.front()-p1.front();
+            float diffY = p2.back()-p1.back();
+            if (diffX == 0) {
+                slope[i][j] = INT_MAX;
+                slope[j][i] = INT_MAX;
+            }else{
+                slope[j][i] = slope[i][j] = diffY/diffX;
+            }
+
+            printf("%d->%d %.3f\n",i,j,slope[i][j]);
+        }
+    }
+    
+    for (int i = 0; i<point.size(); i++) {
+        
+        int pre = i==0?point.size()-1:i-1;
+        float maxSlope, minSlope;
+        if (slope[i][i+1]<slope[i][pre]) {
+            minSlope = slope[i][i+1];
+            maxSlope = slope[i][pre];
+        }else{
+            maxSlope = slope[i][i+1];
+            minSlope = slope[i][pre];
+        }
+        for (int j = 0; j<point.size(); j++) {
+            float val = slope[i][j];
+            if (i!=j && (slope[i][j] < minSlope || slope[i][j] > maxSlope)) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
+
+void permuteUnique(vector<vector<int>> &res, vector<int> &nums, vector<int> &curList, bool *picked){
+    
+    int last = nums.back()+1;
+    int index = 0;
+    
+    for (auto &num:nums){
+        if (!picked[index] && num != last) {
+            picked[index] = true;
+            curList.push_back(num);
+            if (curList.size() == nums.size()) {
+                res.push_back(curList);
+                curList.pop_back();
+                picked[index] = false;
+                return;
+            }
+            permuteUnique(res, nums, curList, picked);
+            
+            last = num;
+            curList.pop_back();
+            picked[index] = false;
+        }
+        index++;
+    }
+}
+
+//16. 带重复元素的排列
+//在当前可选的里面选一个作为下一个，然后推到下一个循环，这样就不会重复。因为在当前位置上，某个种类的数只会出现一次，在当前位置上不会重复。
+vector<vector<int>> permuteUnique2(vector<int> &nums) {
+    if (nums.empty()) {
+        return {{}};
+    }
+    sort(nums.begin(), nums.end());
+    bool picked[nums.size()];
+    memset(picked, 0, sizeof(picked));
+    
+    vector<vector<int>> result;
+    vector<int> curList;
+    permuteUnique(result, nums, curList, picked);
+    
+    return result;
+}
+
+//16. 带重复元素的排列
+//非递归版本，这就是“深度搜索”标签的原因
+/*
+ 1. 深度搜索关键在于回溯，而回溯就需要记住每个选择点，即岔路，的状态，该定义一个什么样的状态又是其中关键问题。
+ 这题里状态就是：当前的路径、已选的数标记和当前选到第几个数
+ 2. 另一个关键点是：向前推进的行为和回溯后的行为要一致，这样才能统一成一个行为，即开始一个新的循环时，你不能区分它是新点还是回溯的旧点。一般就是把新店置为特殊状态的旧点，比如这题,新点就是挖掘0个子节点的点，而旧点就是>0的点，但它们的行为是一致的，就是从未选的数里选一个作为子节点。
+ 3. 第三个关键点就是边界条件：1）什么时候得到完整的路径，即得到一个解 2）什么时候回溯。第一个是在路径最深的时候，指标可以是深度或者没有新的路径了；第二个是当前没有新的路径了，所以**当前选择到哪条路了**是必须的状态
+ */
+vector<vector<int>> permuteUnique(vector<int> &nums) {
+    if (nums.empty()) {
+        return {{}};
+    }
+    sort(nums.begin(), nums.end());
+    bool picked[nums.size()];
+    memset(picked, 0, sizeof(picked));
+    
+    vector<vector<int>> result;
+    vector<int> curList;
+    stack<int> forks;  //这里的索引不是当前节点自己的索引，而是它当前有k个数可以选，从前往后的尝试，下一个可以尝试的数的索引。就是岔路口试到哪一条路了
+    forks.push(0);
+    
+    while (1) {
+        
+        int &index = forks.top();
+        int last = index==0?(nums.back()+1):nums[index-1];
+        bool foundNext = false;
+        
+        while (index < nums.size()){
+            auto num = nums[index];
+            if (!picked[index] && num != last) {
+                foundNext = true;
+                
+                curList.push_back(num);
+                if (curList.size() == nums.size()) {
+                    result.push_back(curList);
+                    
+                    curList.pop_back();
+                    foundNext = false; //虽然找到了，但是只有唯一解，这题的特殊情况，所以回退，用这个变量造个假
+                }else{
+                    picked[index] = true;
+                    index++;
+                    forks.push(0);
+                }
+
+                break;
+            }else{
+                index++;
+            }
+        }
+        
+        if (!foundNext) {
+            curList.pop_back();
+            forks.pop();
+            if (forks.empty()) {
+                return result;
+            }
+            picked[forks.top()-1] = false;
+        }
+    }
+    
+    return result;
+}
+
 int main(int argc, const char * argv[]) {
     
+    vector<int> nums = {1,2,2};
+    auto result = permuteUnique2(nums);
+    printTwoDVector(result);
 }
